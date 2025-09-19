86dc31084c5531bfc6963a95fdfbe5f7d834b73c
Vivekkumar Patel <vivekcanbe@gmail.com>
2025-09-18 22:56:48 -0300

    first practice program implemented

---
+++ labs/lab1/checkNumber.py
@@ -0,0 +1,8 @@
+# 1. Write a program that asks the user for a number and checks if it is positive, negative, or zero. Print the result.
+number=int(input("Please enter the number of your choice   0"))
+if number==0:
+    print("The number is Zero" , number)
+elif number>0:
+    print("The number is Positive", number)
+else:
+    print("The number is negative ", number)
